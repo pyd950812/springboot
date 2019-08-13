afc41e790eef4b215527101cf7bd770f1d7a35db
pyd <939308479@qq.com>
2019-08-13 16:30:26 +0800

    乱码

--- src/main/java/com/pengyd/springboot/dao/GamesDao.java
+++ src/main/java/com/pengyd/springboot/dao/GamesDao.java
@@ -15,30 +15,13 @@ public interface GamesDao {
     List<Games> select(@Param("pojo") Games pojo);
 
     int update(@Param("pojo") Games pojo);
-    /**
-     * 查询最新的游戏 三条
-     * @return
-     */
+
     public List<Games> queryLastGames();
 
-    /**
-     * 查询最火的游戏 五条
-     * @return
-     */
     public List<Games> queryMostHotGames();
 
-
-    /**
-     * 查询推荐的游戏 一条根据查看次数
-     * @return
-     */
     public Games queryRecommendGames();
 
-
-    /**
-     * 查询大型的游戏 三条
-     * @return
-     */
     public List<Games> queryLargeSizeGames();
 
 }
--- src/main/java/com/pengyd/springboot/service/GamesService.java
+++ src/main/java/com/pengyd/springboot/service/GamesService.java
@@ -10,38 +10,17 @@ public interface GamesService {
 
     public int insert(Games pojo);
 
-
     public int insertList(List< Games> pojos);
-
 
     public List<Games> select(Games pojo);
 
-
     public int update(Games pojo);
 
-    /**
-     * 查询最新的游戏 三条
-     * @return
-     */
     public List<Games> queryLastGames();
 
-    /**
-     * 查询最火的游戏 五条
-     * @return
-     */
     public List<Games> queryMostHotGames();
 
-
-    /**
-     * 查询推荐的游戏 一条根据查看次数
-     * @return
-     */
     public Games queryRecommendGames();
 
-
-    /**
-     * 查询大型的游戏 三条
-     * @return
-     */
     public List<Games> queryLargeSizeGames();
 }
